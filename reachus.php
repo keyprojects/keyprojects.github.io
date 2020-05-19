89e0e15e46937229135a65ba9a8eefab4bd239f7
keyprojects <64476673+keyprojects@users.noreply.github.com>
2020-05-20 05:09:40 +0530

    xxx

---
+++ reachus.php
@@ -0,0 +1,21 @@
+<?php
+ $name = $_post['name'];
+  $mail = $_post['email'];
+ $name = $_post['message'];
+
+ 
+ $emil_from='C:\Users\Administrator\Desktop\Folio\index.html';
+ $email_subject ="Message from my site"
+ $email_body= "Name : $name.\n".
+				"Email : $ mail\n".
+				"Message: $$message\n";
+				
+			$to: "pavandpanchal@gmail.com";
+			$headers = "from: $email_from \r\n";
+			$headers = "reply-to: $mail \r\n";
+			mail($to, $email_subject , $email_body, $headers );
+
+			header("Location : index.html");
+			
+			
+ ?>
