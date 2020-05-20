12e92fca47fde51581a1be92eb2a2acfe5d280a5
keyprojects <64476673+keyprojects@users.noreply.github.com>
2020-05-20 06:30:37 +0530

    xx

--- reachus.php
+++ reachus.php
@@ -23,7 +23,21 @@
 			*/
 
 
+<?php
+if(!empty($_POST["send"])) {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
 
+	$toEmail = "pavandpanchal@gmail.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $message, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "reachus.php";
+?>
 
 			
 			
